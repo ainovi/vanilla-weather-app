5c1af318356bbbdfbfc4dfc71bd649d4332589c6
Aino <76224883+ainovi@users.noreply.github.com>
2021-04-29 10:56:13 +0100

    Added the forecast template in JS

--- src/app.js
+++ src/app.js
@@ -15,6 +15,28 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+       <div class="col-2">
+         <div class="weather-forecast-date">${day}</div>
+         <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" />
+         <div class="weather-forecast-temperatures">
+           <span class="weather-forecast-temperature-max"> 18° </span>
+           <span class="weather-forecast-temperature-min"> 12° </span>
+         </div>
+     </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -70,6 +92,8 @@ function showCelciusTemperature(event) {
 }
 
 let celciusTemperature = null;
+
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
